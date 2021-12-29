fe40f32c09a0131e9ad8dd1c3d5da4169ae0d64b
host4ideas <felixmb99@hotmail.es>
2021-12-29 13:03:42 +0100

    updated mongoose exercise

--- Web_development/mongoDB/Mongoose/app.js
+++ Web_development/mongoDB/Mongoose/app.js
@@ -3,20 +3,45 @@ const { Schema } = mongoose;
 const { model } = mongoose;
 const { log } = console;
 
-// // Create connection
-const connection = mongoose.connect('mongodb://localhost:27017/fruitsDB');
+// Create connection
+mongoose.connect('mongodb://localhost:27017/fruitsDB');
 
-// // Foundation for every new fruits added
+// Foundation for every new fruits added
 const fruitSchema = new Schema({
-    name: String,
-    rating: Number,
+    name: {
+        type: String,
+        required: [true, `Don't have ideas?`]
+    },
+    rating: {
+        type: Number,
+        min: [1, 'At least one star please'],
+        max: [10, '{VALUE} is to much love!']
+    },
     review: String
 });
 
-// // Create the model (constructor) from the schema
+// Create the model (constructor) from the schema
 const Fruit = model('Fruit', fruitSchema);
 
-// // New document (instance of Fruit)
+// New document (instance of Fruit)
+const apple = new Fruit({
+    name: 'Apple',
+    rating: 20,
+    review: "The best fruit."
+});
+// FOR TESTING VALIDATIONS
+// apple.save().then(result => {
+//     log(result);
+// }, err => {
+//     log(err.message);
+// })
+
+// apple.save().then(result => {
+//     log(result);
+// }).catch(err => {
+//     log(err.message);
+// });
+
 const kiwi = new Fruit({
     name: "Kiwi",
     rating: 10,
@@ -35,25 +60,92 @@ const banana = new Fruit({
     review: "Weird texture."
 });
 
-// Insert document into fruits collection
-Fruit.insertMany([kiwi, orange, banana], (err) => {
-    if (err) log(err); 
-    else log('Saved fruits');
-});
-
 const personSchema = new Schema({
     name: String,
-    age: Number
+    age: Number,
+    favoriteFruit: fruitSchema
 });
 
 const Person = model('Person', personSchema);
 
-const person = new Person({
+const pineapple = new Fruit({
+    name: 'Pineapple',
+    rating: 9.5,
+    review: 'Great fruit'
+})
+
+const avocado = new Fruit({
+    name: 'Avocado',
+    rating: 7.5,
+    review: 'Better a bit unripe'
+})
+
+const john = new Person({
     name: 'John',
-    age: 37
+    age: 37,
+    favoriteFruit: avocado
 });
 
-person.save().then(result => {
+// RELATIONSHIP PERSON - FRUIT
+
+const amy = new Person({
+    name: 'Amy',
+    age: 12,
+    favoriteFruit: pineapple
+});
+
+// pineapple.save();
+// amy.save().then(result => {
+//     log(result);
+//     mongoose.connection.close();
+// });
+
+// avocado.save();
+Person.updateOne({ name: 'John' }, { favoriteFruit: avocado }).then(result => {
+    log(result)
+}, err => {
+    log(err)
+});
+
+// john.save().then(result => {
+//     log(result);
+//     mongoose.connection.close();
+// });
+
+// Insert document into fruits collection
+// Fruit.insertMany([apple, kiwi, orange, banana], (err) => {
+//     if (err) log(err);
+//     else log('Saved fruits');
+// });
+
+// UPDATE
+// Fruit.updateOne({ name: 'Apple' }, { name: 'Peach' }).then(result => {
+//     log(result);
+// }, err => {
+//     log(err);
+// })
+
+// DELETE
+Fruit.deleteOne({ name: 'Peach' }).then(result => {
     log(result);
-    mongoose.connection.close();
+}, err => {
+    log(err);
+});
+
+Fruit.find().then(result => {
+    result.forEach(fruit => {
+        log(fruit.name);
+    })
+});
+
+// Person.deleteMany({ name: 'John' }).then(result => {
+//     log(result)
+// }, err => {
+//     log(err)
+// });
+
+Person.find().then(result => {
+    result.forEach(fruit => {
+        log(fruit.name);
+    })
 });
