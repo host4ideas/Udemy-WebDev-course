24ae8db0638becfd50dbac238118276ea7cde39a
host4ideas <felixmb99@hotmail.es>
2021-12-31 13:59:00 +0100

    started wiki REST app

--- Exercises/todolist-v2-mongo/app.js
+++ Exercises/todolist-v2-mongo/app.js
@@ -18,95 +18,98 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 const itemSchema = new Schema({
-    name: String
+	name: String
 });
 
 const Item = model("Item", itemSchema);
 
 const item1 = new Item({
-    name: "Item 1"
+	name: "Item 1"
 });
 
 const item2 = new Item({
-    name: "Item 2"
+	name: "Item 2"
 });
 
 const item3 = new Item({
-    name: "Item 3"
+	name: "Item 3"
 });
 
 const defaultItems = [item1, item2, item3];
 
 const listSchema = {
-    name: String,
-    items: [itemSchema]
+	name: String,
+	items: [itemSchema]
 };
 
 const List = new model('List', listSchema);
 
 app.get("/", function (req, res) {
-    res.redirect("/Today");
+	res.redirect("/Today");
 });
 
 app.get('/:customList', (req, res) => {
-    let customListName = _.kebabCase(req.params.customList);
-    customListName = _.capitalize(customListName);
+	let customListName = _.kebabCase(req.params.customList);
+	customListName = _.capitalize(customListName);
 
-    List.find({ name: customListName }).then(result => {
-        if (result.length < 1) {
-            // Create a new list
-            const list = new List({
-                name: customListName,
-                items: defaultItems
-            });
+	List.find({ name: customListName }).then(result => {
+		if (result.length < 1) {
+			// Create a new list
+			const list = new List({
+				name: customListName,
+				items: defaultItems
+			});
 
-            list.save().then(result => {
-                res.redirect(`/${customListName}`)
-            }, err => {
-                log(err);
-            });
-        } else {
-            // Show existing list
-            res.render('list', { listTitle: customListName, newListItems: result[0].items });
-        }
-    }, err => {
-        log(err);
-    });
+			list.save().then(result => {
+				res.redirect(`/${customListName}`)
+			}, err => {
+				log(err);
+			});
+		} else {
+			// Show existing list
+			res.render('list', { listTitle: customListName, newListItems: result[0].items });
+		}
+	}, err => {
+		log(err);
+	});
 });
 
 app.post('/', (req, res) => {
-    let item = new Item({
-        name: req.body.newItem
-    });
+	let item = new Item({
+		name: req.body.newItem
+	});
 
-    List.findOneAndUpdate({ name: req.body.list }, { $push: { items: item } }).then(result => {
-        log(req.body.list);
-        res.redirect(`/${req.body.list}`);
-    }, err => {
-        log(err);
-    });
+	List.findOneAndUpdate({ name: req.body.list }, { $push: { items: item } }).then(result => {
+		log(req.body.list);
+		res.redirect(`/${req.body.list}`);
+	}, err => {
+		log(err);
+	});
 });
 
 app.post('/delete', (req, res) => {
-    const listName = req.body.listName;
-    const elementId = req.body.checkbox;
-    log(elementId);
+	const listName = req.body.listName;
+	const elementId = req.body.checkbox;
+	log(elementId);
 
-    List.findOneAndUpdate({ name: listName }, { $pull: { items: { _id: elementId } } }).then(result => {
-        res.redirect("/" + listName);
-    }, err => {
-        log(err);
-    });
+	List.findOneAndUpdate({ name: listName }, { $pull: { items: { _id: elementId } } }).then(result => {
+		res.redirect("/" + listName);
+	}, err => {
+		log(err);
+	});
 });
 
 app.get("/about", function (req, res) {
-    res.render("about");
+	res.render("about");
 });
 
 if (port == null || port == "") {
-    port = 3000;
+	port = 3000;
 }
 
+// If the port doens't exist in the .env file (development environment),
+// the port will be provided by Heroku
 app.listen(port, (req, res) => {
-    log(`Server running at: http://localhost:${port}`)
+	// For development use only
+	log(`Server running at: http://localhost:${port}`)
 });
--- Exercises/website-blog/app.js
+++ Exercises/website-blog/app.js
@@ -1,62 +1,98 @@
-//jshint esversion:6
+require('dotenv').config();
 const { log } = require('console');
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
 const _ = require('lodash');
-const { render } = require('express/lib/response');
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const { model } = mongoose;
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
+let port = process.env.PORT;
 const app = express();
-
-const posts = [];
-
-let testPost = {
-  title: "Test",
-  content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod esse, minima modi, cumque ex, soluta accusamus ab explicabo doloremque doloribus nisi quis quia ad expedita magnam sint facere saepe. Dolorem.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod esse, minima modi, cumque ex, soluta accusamus ab explicabo doloremque doloribus nisi quis quia ad expedita magnam sint facere saepe. Dolorem.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod esse, minima modi, cumque ex, soluta accusamus ab explicabo doloremque doloribus nisi quis quia ad expedita magnam sint facere saepe. Dolorem."
-};
-
-posts.push(testPost);
-
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// MongoDB connection
+mongoose.connect("mongodb://localhost:27017/blogDB");
+
+// MongoDB Schema
+const postSchema = new Schema({
+	title: 'String',
+	content: 'String'
+});
+// MongoDB model
+const Post = new model('Post', postSchema);
+
+// Pages content
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const posts = [];
+
 app.get('/', (req, res) => {
-  res.render('home', { startingContent: homeStartingContent, posts: posts, lodash: _ });
+	Post.find().then(result => {
+		res.render('home', { startingContent: homeStartingContent, posts: result, lodash: _ });
+	});
 });
 
 app.get('/about', (req, res) => {
-  res.render('about', { aboutContent: aboutContent });
+	res.render('about', { aboutContent: aboutContent });
 });
 
 app.get('/contact', (req, res) => {
-  res.render('contact', { contactContent: contactContent });
+	res.render('contact', { contactContent: contactContent });
 });
 
 app.get('/compose', (req, res) => {
-  res.render('compose');
+	res.render('compose');
 });
 
 app.get('/posts/:article', (req, res) => {
-  let post = posts.find(post => _.kebabCase(post.title) == req.params.article);
+	// Since the name in DB is kebab case and the title shouldn't,
+	// first we convert the input into kebab case, search,
+	// and then show it not kebab case
+	let postTitle = _.kebabCase(req.params.article);
 
-  if (post != undefined) { res.render('post', { postTitle: post.title, postContent: post.content }) }
+	Post.find({ title: postTitle }).then(result => {
+		if (result.length > 0)
+			res.render('post', { postTitle: _.capitalize(result[0].title.split('-').join(' ')), postContent: result[0].content });
+	}, err => {
+		log(err);
+	})
 });
-
+// When a post is submitted, format to kebab case the title
 app.post('/compose', (req, res) => {
-  let post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  }
-  posts.push(post);
-  res.redirect('/');
+	const postTitle = _.kebabCase(req.body.postTitle);
+
+	Post.find({ title: postTitle }).then(result => {
+		if (result.length > 0) {
+			// If the post exist redirect to that post
+			res.render('post', { postTitle: _.capitalize(result[0].title.split('-').join(' ')), postContent: result[0].content });
+			res.redirect(`/posts/${result[0].title}`);
+		} else {
+			// If the post doesn't exits, create a new post
+			let post = new Post({
+				title: _.kebabCase(req.body.postTitle),
+				content: req.body.postBody
+			});
+
+			post.save((err, result) => {
+				(!err) ? res.redirect(`/posts/${postTitle}`) : log(err);
+			});
+		}
+	}, err => {
+		log(err);
+	})
 });
 
-app.listen(3000, function () {
-  log(`Server running at: http://localhost:3000`);
+// If the port doens't exist in the .env file (development environment),
+// the port will be provided by Heroku
+if (port == null || port == "") {
+	port = 3000;
+}
+
+app.listen(port, function () {
+	// For development use only
+	log(`Server running at: http://localhost:3000`);
 });
